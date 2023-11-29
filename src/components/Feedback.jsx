e38f35b1036bacf716e4cdcbf1e53444e997d932
thursdaycurry <thursdaycurry@gmail.com>
2023-11-29 19:29:50 +0900

    create .env

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+
+const StarRating = ({ count, value, onChange }) => {
+  return (
+    <div>
+      {Array.from({ length: count }, (_, i) => i + 1).map((idx) => (
+        <span
+          key={idx}
+          className={`cursor-pointer ${
+            idx <= value ? 'text-yellow-500' : 'text-gray-300'
+          }`}
+          onClick={() => onChange(idx)}
+        >
+          ★
+        </span>
+      ))}
+    </div>
+  );
+};
+
+const FeedbackModal = ({ isOpen, onClose }) => {
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // const formId = process.env.FORMID;
+    // const formEntryRating = process.env.FORMENTRYRATING;
+    // const formEntryComment = process.env.FORMENTRYCOMMENT;
+
+    const formId = process.env.REACT_APP_FORMID;
+    const formEntryRating = process.env.REACT_APP_FORMENTRYRATING;
+    const formEntryComment = process.env.REACT_APP_FORMENTRYCOMMENT;
+    const googleFormUrl = `https://docs.google.com/forms/d/e/${formId}/formResponse`;
+
+    const formData = new FormData();
+    formData.append(formEntryRating, rating);
+    formData.append(formEntryComment, comment);
+
+    try {
+      const response = await fetch(googleFormUrl, {
+        method: 'POST',
+        body: formData,
+        mode: 'no-cors', // Necessary to avoid CORS error
+      });
+
+      // The response is opaque due to 'no-cors', so we can't check if it was successful
+      console.log('Form submitted');
+      onClose(); // Close the modal
+    } catch (error) {
+      console.error('Submission error', error);
+    }
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className='fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full'>
+      <div className='relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white'>
+        <div className='mt-3 text-center'>
+          <h3 className='text-lg leading-6 font-medium text-gray-900'>
+            Feedback
+          </h3>
+          <div className='mt-2 px-7 py-3'>
+            <StarRating
+              count={5}
+              value={rating}
+              onChange={(value) => setRating(value)}
+            />
+            <textarea
+              className='mt-1 p-2 w-full rounded-md border'
+              rows='4'
+              placeholder='Leave a comment...'
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            ></textarea>
+          </div>
+          <div className='items-center px-4 py-3'>
+            <button
+              className='px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300'
+              onClick={handleSubmit}
+            >
+              Submit Feedback
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedbackModal;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+import FeedbackModal from './Feedback';
+
 const Footer = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <footer className='bg-gray-100 mt-10 p-4 text-center text-sm text-gray-600'>
       <div className='container mx-auto'>
         <div>
           &copy; {new Date().getFullYear()} CiteR!ght. All rights reserved.
         </div>
-        <div>Developed by Thursdaycurry</div>
+        <div className='my-1'>Developed by Thursdaycurry</div>
+
+        {/* Links */}
         <div className='inline-flex'>
           User voice {': '}
           <a
@@ -35,6 +42,20 @@ const Footer = () => {
             src='https://platform.twitter.com/widgets.js'
             charset='utf-8'
           ></script>
+        </div>
+
+        {/* Feedback modal */}
+        <div className='my-1'>
+          <button
+            className='px-4 py-1 text-white bg-gray-500 rounded hover:bg-gray-700'
+            onClick={() => setModalOpen(true)}
+          >
+            Open Feedback
+          </button>
+          <FeedbackModal
+            isOpen={modalOpen}
+            onClose={() => setModalOpen(false)}
+          />
         </div>
       </div>
     </footer>
